cd275277ac7cc7ade54e18750eeb9408819af986
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2022-02-08 17:29:08 +0800

    First file
    Signed-off-by: NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>

---
+++ python/1.py
@@ -0,0 +1,12 @@
+age = int(input('Enter age: '))
+
+if (age > 59):
+    print("You are a old man")
+elif age > 30:
+    print("You are an adult")
+elif age > 14:
+    print("You are a youth")
+elif age > 12:
+    print("You are a teenager")
+else:
+    print("You are still a child")
